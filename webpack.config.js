98912bc8bac53e1ce0ffb7f872d2454b9ce52062
Whidy_Macbook <whidy@whidy.net>
2019-07-09 11:31:09 +0800

    更新到webpack4

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,68 @@
-module.exports = function (env) {
-  return require('./webpack.' + env + '.js');
-}
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const IS_DEV = process.env.NODE_ENV === 'dev';
+module.exports = {
+  mode: IS_DEV ? 'development' : 'production',
+  devtool: IS_DEV ? 'eval' : 'source-map',
+  entry: {
+    index: path.resolve(__dirname, 'src/index.js')
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [{
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          IS_DEV ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [{
+          loader: 'url-loader',
+          options: {
+            limit: 4096
+          }
+        }]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html',
+      inject: 'head'
+    }),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery'
+    }),
+    new MiniCssExtractPlugin({
+      filename: IS_DEV ? '[name].css' : '[name].[hash].css',
+      chunkFilename: IS_DEV ? '[id].css' : '[id].[hash].css',
+    }),
+    new CleanWebpackPlugin()
+  ],
+  devServer: {
+    // contentBase: __dirname,
+    compress: true,
+    port: 9000,
+    inline: true,
+    host: '0.0.0.0',
+    disableHostCheck: true
+  }
+};
